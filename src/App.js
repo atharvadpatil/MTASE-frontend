025bcf68f78991ec475c7198f6c3d80a02404a05
Viraj Patidar <vrj381@gmail.com>
2022-02-21 21:15:02 +0530

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import Test from './components/test'
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from './components/navbar';
+import NotFound from './components/notFound';
+import Home from './components/home';
+import Summarize from './components/summarize';
+import Test from './components/test';
+import Approach from './components/approach';
+
+// MUI
+import { blue } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: blue[900],
+        },
+    },
+});
+
 
 function App() {
     return (
-        <div>
-            <Test />
-        </div>
+        <ThemeProvider theme={theme}>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} exact />
+
+                <Route path="/summarize" element={<Summarize />} exact />
+                <Route path="/approach" element={<Approach />} exact />
+                <Route path="/test" element={<Test />} exact />
+
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </ThemeProvider>
     );
 }
 
