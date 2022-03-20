a9f17abd2f2ea4cdfdec3c96f5c12e06eab96895
Viraj Patidar <vrj381@gmail.com>
2022-03-20 19:43:09 +0530

    text input completed + loading

--- src/components/summarize.js
+++ src/components/summarize.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import FileUpload from "react-material-file-upload";
+import { useNavigate } from 'react-router-dom';
+import { results } from '../atoms';
+import { useSetRecoilState } from 'recoil';
 
 //MUI
 import Box from '@mui/material/Box';
-import { Button, Grid, TextField, Typography } from '@mui/material';
+import { Grid, TextField, Typography } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
 import { lightBlue } from '@mui/material/colors';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
@@ -18,11 +22,16 @@ const theme = createTheme({
 
 const Summarize = () => {
 
-    const [file, setFile] = useState([]);
-    const [fileError, setFileError] = useState(false);
+    const navigate = useNavigate();
 
-    const [text, setText] = useState("");
+    const [file, setFile] = useState([])
+    const [fileError, setFileError] = useState(false)
+
+    const [text, setText] = useState("")
     const [textError, setTextError] = useState(false)
+
+    const [loading, setLoading] = useState(false)
+    const setResult = useSetRecoilState(results)
 
 
     const handleSubmit = e => {
@@ -39,6 +48,7 @@ const Summarize = () => {
         }
 
         if (submit) {
+            setResult({});
             let form_data = new FormData();
             if (text !== "") {
                 form_data.append('text', text);
@@ -47,13 +57,21 @@ const Summarize = () => {
                 form_data.append('name', file[0].name);
                 form_data.append('file', file[0]);
             }
+            setLoading(true);
             axios.post(`http://localhost:8000/api/summarise`, form_data)
                 .then((res) => {
+                    setLoading(false);
                     console.log(res);
-                    alert(res.data.text);
+                    setResult(res.data);
+                    navigate("/results");
+                    // alert(res.data.text);
                     // window.location.reload();
                 })
-                .catch(err => console.log(err));
+                .catch(err => {
+                    console.log(err);
+                    setLoading(false);
+                    alert("An error occured! Please try again.")
+                });
         }
 
     }
@@ -94,9 +112,17 @@ const Summarize = () => {
                         </ThemeProvider>
                     </Box>
                     <Box mt={3}>
-                        <Button type="submit" fullwidth onClick={handleSubmit} variant="contained" color="primary" sx={{ width: '100%' }}>
+                        <LoadingButton
+                            loading={loading}
+                            loadingIndicator="Processing..."
+                            variant="contained"
+                            type="submit" 
+                            onClick={handleSubmit} 
+                            color="primary" 
+                            sx={{ width: '100%' }}
+                        >
                             Submit
-                        </Button>
+                        </LoadingButton>
                     </Box>
                 </Grid>
             </Grid>
